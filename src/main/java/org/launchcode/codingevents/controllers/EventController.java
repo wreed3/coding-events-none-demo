b0bbde16ea5710eaff02047b6512e31c7fac8339
wreed3rd <wreed3rd@gmail.com>
2022-02-20 22:57:35 -0500

    Creating a new branch for Exercise 13.5

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,10 +5,10 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("events")
@@ -24,12 +24,18 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model){
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
 
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent){
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+
+        }
         EventData.add(newEvent);
         return "redirect:";
 
@@ -55,6 +61,7 @@ public class EventController {
     @GetMapping("edit/{eventId}")
     public String displayEditForm(Model model, @PathVariable(required = false) int eventId){
        Event event = EventData.getById(eventId);
+       model.addAttribute(event);
         model.addAttribute("title","Edit Event " + event.getName() + " id- " +event.getId());
         return "/events/edit";
     }
